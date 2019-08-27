089b2fc385736951cfdac8c6094342ae932249d1
Viko1 <vitaliiropiak@gmail.com>
2019-08-27 23:15:52 +0200

    Kitchen blog v1.61 categories

--- kitchen_blog/blog/admin.py
+++ kitchen_blog/blog/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Category
 
 # Register your models here.
 admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+admin.site.register(Category, CategoryAdmin)
--- kitchen_blog/blog/models.py
+++ kitchen_blog/blog/models.py
@@ -3,19 +3,39 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 
 
+
+#Category
+class Category(models.Model):
+    name = models.CharField (max_length=240, blank=True, null=True, default='Category')
+    slug = models.SlugField (max_length=240, unique=True)
+
+    class Meta:
+        ordering = ('name', )
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __set__(self):
+        return self.name
+
 STATUS = (
     (0,"Draft"),
     (1,"Publish")
 )
 
+
+
+
+#Post
 class Post(models.Model):
-    title = models.CharField(max_length=255)
+
+    title = models.CharField(max_length=255, blank=True, null=True, default='Post')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     image = models.ImageField(upload_to='blog_images')
     updated_on = models.DateTimeField(auto_now=True)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
